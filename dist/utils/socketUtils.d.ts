56c9778fd0861eecbe7410d8af34986c979973a3
Erfan <143953545+bigerfan@users.noreply.github.com>
2025-09-23 10:59:13 +0330

    first commit

--- dist/utils/socketUtils.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const formatedRooms: (rooms: Map<string, Set<string>>) => {
-    tableId: string | undefined;
-    users: string[];
-    count: number;
-}[];
-//# sourceMappingURL=socketUtils.d.ts.map
